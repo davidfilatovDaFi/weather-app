55b64791c2ef429fdd54a84bee219722825dfc9d
davidfilattovDaFi <oxelolol@gmail.com>
2023-03-13 18:06:36 +0300

    upgraded search

--- src/App.jsx
+++ src/App.jsx
@@ -9,37 +9,57 @@ import useTime from "./hooks/useTime";
 function App() {
   const time = useTime()
   const [city,setCity] = useState('Moscow')
+  const [cityList,setCityList] = useState([])
   const [cityName,setCityName] = useState('Moscow')
   const [weather,setWeather] = useState([])
   const [current,setCurrent] = useState(0)
-  const [fetching,loader,error] = useFetch(async () => {
-    const link = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=a2cd589df125dc8cbd69ea767fbb9c3c`
+  const [selectCity,setSelectCity] = useState('')
+  const [fetchingCity,loaderCity,errorCity] = useFetch(async () => {
+    const link = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=a2cd589df125dc8cbd69ea767fbb9c3c`
     const response = await fetch(link)
     const data = await response.json()
-    const lat = data[0].lat
-    const lon = data[0].lon
+    const filterdata = data.filter(el => el.name.toLowerCase() === city.toLowerCase())
+    setCityList([...new Set(data.map(city => city.name))]
+      .filter(el => el.toLowerCase().includes(city.toLowerCase())))
+    const lat = filterdata[0].lat
+    const lon = filterdata[0].lon
+    console.log(data)
+    return [filterdata,lat,lon]
+  })
+  const [fetching,loader,error] = useFetch(async () => {
+    const [data,lat,lon] = await fetchingCity()
     const response2 = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=a2cd589df125dc8cbd69ea767fbb9c3c&units=metric`)
     const data2 = await response2.json()
-    console.log(data[0].name)
     const filterdata = data2.list.filter(el => el.dt_txt.includes('15:00:00'))
     return [filterdata,data[0].name]
   })
 
   const fetchWeather = useCallback(async () => {
-    setWeather([...await fetching()][0])
+    setWeather([...await fetching(city)][0])
     setCityName([...await fetching()][1])
-    console.log(weather)
   },[city])
 
   useEffect(() => {
+    fetchingCity()
+  }, [city])
+  
+  useEffect(() => {
     fetchWeather()
-  },[])
+    setCity('')
+  },[selectCity])
   
   return (
     <div className="App">
         <Header city={cityName}/>
         <main>
-          <Form city={city} getName={setCityName} getValue={setCity} fetchWeather={fetchWeather}/>
+          <Form 
+            city={city} 
+            getName={setCityName} 
+            getValue={setCity} 
+            fetchWeather={fetchWeather} 
+            cityList={cityList}
+            selectCity={setSelectCity}
+          />
           <Clock time={time}/>
           <List 
           setCurrent={setCurrent} 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,32 @@
 import React, { memo } from 'react'
 
-const Form = memo(({fetchWeather,getValue,getName,city}) => {
+const Form = memo(({fetchWeather,getValue,getName,city,cityList,selectCity}) => {
   return (
     <form onSubmit={e => {
       e.preventDefault()
       if (city.length !== 0) {
         fetchWeather()
-        getName(city)
       } 
       getValue('')
       console.log(city)
-    }} className="find">
-      <input value={city} onChange={e => getValue(e.target.value)} className="find__name" type="text" placeholder="Type city..." />
-      <button className="find__button">Find</button>
+    }} className="form">
+      <div className="form__find find">
+        <input value={city} onChange={e => {
+          console.log(city)
+          getValue(e.target.value)
+        }} className="find__name" type="text" placeholder="Type city..." />
+        <button className="find__button">Find</button>
+      </div>
+      <ul className={(city.length && cityList.length) === 0 ? 'form__list none' : 'form__list'}>
+        {cityList.map(city => <li 
+        key={city}
+        className='form__item'
+        onClick={() => {
+          getValue(city)
+          selectCity(city)
+        }}
+        >{city}</li>)}
+      </ul>
     </form>
   )
 })
